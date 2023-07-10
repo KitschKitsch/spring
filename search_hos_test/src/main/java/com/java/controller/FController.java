0de66c2bd30e35df70213b792df370e92d2da55d
KitschKitsch <jss5794@naver.com>
2023-07-10 19:20:45 +0900

    카카오맵api

---
+++ search_hos_test/src/main/java/com/java/controller/FController.java
@@ -0,0 +1,23 @@
+package com.java.controller;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FController {
+	
+	
+	@RequestMapping("/index2")
+	public String index2() {
+		return "index2";
+	}
+	
+
+}
