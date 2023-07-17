34c1338febd621e86f908a1658cadbf4a20f270b
KitschKitsch <jss5794@naver.com>
2023-07-17 23:14:15 +0900

    test pjt

---
+++ ys_test2/src/main/java/com/java/controller/MadangController.java
@@ -0,0 +1,13 @@
+package com.java.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MadangController {
+
+	@RequestMapping("/madangs_folder/madang_1")
+	public String madang_1() {
+		return "madangs_folder/madang_1";
+	}
+}
