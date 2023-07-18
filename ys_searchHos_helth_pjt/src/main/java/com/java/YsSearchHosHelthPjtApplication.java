5c2b776badb8c44fa1a4054951b78a262bf8c3be
KitschKitsch <jss5794@naver.com>
2023-07-18 23:52:45 +0900

    0718# WARNING: head commit changed in the meantime
    Merge branch 'master' of https://github.com/KitschKitsch/spring.git

---
+++ ys_searchHos_helth_pjt/src/main/java/com/java/YsSearchHosHelthPjtApplication.java
@@ -0,0 +1,13 @@
+package com.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YsSearchHosHelthPjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YsSearchHosHelthPjtApplication.class, args);
+	}
+
+}
--- ys_test2/src/main/java/com/java/service/MadangServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.java.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class MadangServiceImpl implements MadangService {
-
-}
